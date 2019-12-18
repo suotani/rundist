c953c36ef331611425883c369df12ba85968b66c
root <root@DESKTOP-GSID79E.localdomain>
2019-12-18 22:20:47 +0900

    fix type miss

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -37,7 +37,7 @@ class WorksController < ApplicationController
 
   def check_user
     if session[:user_id].blank?
-      user = User.find_by(room_name: params[:user_id])
+      user = User.find_by(custom_url: params[:user_id])
       redirect_to root_path if user.blank?
       session[:user_id] = user.id
     end
