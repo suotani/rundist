6d1e1aec731da37b6c065961eb0347bed1727a2a
root <root@DESKTOP-GSID79E.localdomain>
2019-12-18 22:26:49 +0900

    fix user bug

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -40,6 +40,7 @@ class WorksController < ApplicationController
       user = User.find_by(custom_url: params[:user_id])
       redirect_to root_path if user.blank?
       session[:user_id] = user.id
+      @current_user = user
     end
   end
 end
